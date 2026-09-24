da3d043aabdab32f6b98200065f8439aa90e2f94
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2026-09-25 01:19:24 +0300

    starting up onto the javascript

---
+++ Week 4/DAY1/script.js
@@ -0,0 +1,5 @@
+const name = "Derrick"
+const age =28
+const course = "Software engineering"
+const country = "Uganda"
+console.log(`My name is ${name} and am ${age} doing my ${course} i come from ${country}`);
