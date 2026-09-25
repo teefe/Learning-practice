f74b53dcee91e8781df849bd565e81b455caa681
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2026-09-25 12:39:21 +0300

    challenge 2 completed

--- Week 4/DAY1/script.js
+++ Week 4/DAY1/script.js
@@ -1,5 +1,13 @@
+//challenge 1
+console.log("Personal Information");
 const name = "Derrick"
 const age =28
 const course = "Software engineering"
 const country = "Uganda"
 console.log(`My name is ${name} and am ${age} doing my ${course} i come from ${country}`);
+//Challange 2
+console.log("Shopping Calculator");
+let productPrice = 20000;
+let quantity = 15;
+let totalPrice = productPrice * quantity;
+console.log(`Product price is ${productPrice} and quantity of ${quantity}, so total amount is ${totalPrice}`);
