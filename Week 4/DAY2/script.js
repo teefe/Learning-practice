89e6ab13b16c6dd885592dbb5fa77861a6da24de
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2026-09-26 19:40:09 +0300

    challenge 4 done

--- Week 4/DAY2/script.js
+++ Week 4/DAY2/script.js
@@ -5,6 +5,7 @@ if (age > 18){
 }else{
     console.log("You are too young to register");
 }
+
 console.log("Day 2 Challenge 2, Login Check");
 const username = "Derrick";
 const password = "123456";
@@ -12,4 +13,20 @@ if (username === "Derrick" && password === "123456"){
     console.log("Login successfull.");
 }else{
     console.log("Invalid credentials");
+}
+
+console.log("Challenge 3, Study status");
+const studyHours = 1;
+if (studyHours >= 5)
+    {
+    console.log("Excellent study day");
+}else if (studyHours >= 3 && studyHours <= 4 )
+    {
+    console.log("Good study day");
+}else if(studyHours >= 1 && studyHours <= 2)
+    {
+    console.log("Keep improving");
+}else
+    {
+        console.log("You need to focus");
 }
