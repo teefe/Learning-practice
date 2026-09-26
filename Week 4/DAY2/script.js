e59c01e8e7f3c6147eda20856c8c5f4fa163ecef
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2026-09-26 18:01:17 +0300

    Day 2 Challenge 2 completed

---
+++ Week 4/DAY2/script.js
@@ -0,0 +1,15 @@
+console.log("Day 2 Challenge 1, Age Check");
+const age = "28"
+if (age > 18){
+    console.log("You can Register");
+}else{
+    console.log("You are too young to register");
+}
+console.log("Day 2 Challenge 2, Login Check");
+const username = "Derrick";
+const password = "123456";
+if (username === "Derrick" && password === "123456"){
+    console.log("Login successfull.");
+}else{
+    console.log("Invalid credentials");
+}
